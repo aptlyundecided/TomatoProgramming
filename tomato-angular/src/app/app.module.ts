279fdc01eab1688867623834eac57068351da9c0
Alex <adwilson0286@gmail.com>
2020-09-11 14:16:52 -0400

    :construction_worker: prepare app for first component

--- tomato-angular/src/app/app.module.ts
+++ tomato-angular/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+
+// [NgRx]
+import { StoreModule } from '@ngrx/store'
+import { reducers } from './store/reducers/index.reducer'
+// import { FirstReducer } from './ngrx/reducers/first-reducer.reducer'
+// import { selectProgrammerCoffeeSips } from './ngrx/selectors/first-selector.selector';
+import { FirstNgrxComponentComponent } from './panels/first-ngrx-component/first-ngrx-component.component'
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstNgrxComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({
+      programmer: reducers.programmer
+    }, {}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
