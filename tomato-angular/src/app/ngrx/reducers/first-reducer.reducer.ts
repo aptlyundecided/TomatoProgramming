e78ecafe945e4456773cc5112df506ce9ca1e67e
Alex <adwilson0286@gmail.com>
2020-09-11 10:12:24 -0400

    :construction_worker: just setting up a template reducer

--- tomato-angular/src/app/ngrx/reducers/first-reducer.reducer.ts
+++ tomato-angular/src/app/ngrx/reducers/first-reducer.reducer.ts
@@ -1,17 +1,16 @@
-/*
-TUTORIAL CODE
-
-import { Action, createReducer, on } from '@ngrx/store';
-import * as ScoreboardPageActions from '../actions/scoreboard-page.actions';
-
-export interface State {
-  home: number;
-  away: number;
-}
-
-*/
-
-import { Action } from '@ngrx/store'
+/*]
+|| ----------------------------------------------------------- ||
+    MY FIRST REDUCER
+    ----------------------------------------------------------
+    OK so it's not truly my first reducer, but it's the first
+    time I am really going to take my time and TRULY
+    try to understand everything that's happening in here.
+    ----------------------------------------------------------
+    Born On:    11.September.2020
+    Author:     Alex Wilson
+|| ----------------------------------------------------------- ||
+[*/
+import { Action, createReducer, on} from '@ngrx/store'
 /*]
 [|] TODO|AW: Define what this is really doing.
 [*/
@@ -22,4 +21,30 @@ export interface State {
 }
 /*]
 [|]
+[*/
+const initialState: State = {
+  clicks: 0,
+  coffee_sips: 0,
+  random_numbers: []
+}
+/*]
+[|] NOTE:
+[|] Rather than using a switch case like redux, this thing seems to have it's own
+[|] built-in handling???  Looks like I need some pre-made actions.
+[*/
+const scoreboardReducer = createReducer(
+  initialState,
+  // on(ScoreboardPageActions.homeScore, state => ({ ...state, home: state.home + 1 })),
+  // on(ScoreboardPageActions.awayScore, state => ({ ...state, away: state.away + 1 })),
+  // on(ScoreboardPageActions.resetScore, state => ({ home: 0, away: 0 })),
+  // on(ScoreboardPageActions.setScores, (state, { game }) => ({ home: game.home, away: game.away }))
+)
+/*]
+[|]
+[*/
+export function reducer(state: State | undefined, action: Action) {
+  return scoreboardReducer(state, action)
+}
+/*]
+[E] END
 [*/
