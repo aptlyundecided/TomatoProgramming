898942f1169f4b44ed3cffb8806b05fab0a4e71e
Alex <adwilson0286@gmail.com>
2020-09-11 14:16:14 -0400

    :construction_worker: create route to the first page

--- tomato-angular/src/app/app-routing.module.ts
+++ tomato-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [];
+import { FirstNgrxComponentComponent } from './panels/first-ngrx-component/first-ngrx-component.component'
+
+const routes: Routes = [
+  { path: '', component: FirstNgrxComponentComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
